e4c85b8ecf07491162ef5321d177e2889ea4f8cd
hamzaalictn <r.hmzctn@gmail.com>
2020-12-20 08:43:29 +0100

    Frame Work

---
+++ src/test/java/inaracademy/MavenJava/AppiumTest.java
@@ -0,0 +1,14 @@
+package inaracademy.MavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void portJira() {
+
+		System.out.println("portJira");
+		 
+	}
+}
---
+++ src/test/java/inaracademy/MavenJava/RESTAPITest.java
@@ -0,0 +1,13 @@
+package inaracademy.MavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+
+	}
+}
